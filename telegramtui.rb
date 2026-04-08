f14259e8a7024f650c40e594baf7027310f3a1dc
k4dy <konrad.mady@gmail.com>
2026-04-08 11:14:58 +0200

    TelegramTUI v1.0.0

---
+++ telegramtui.rb
@@ -0,0 +1,30 @@
+class Telegramtui < Formula
+  desc "Telegram TUI client for the terminal, inspired by lazygit"
+  homepage "https://github.com/k4dy/telegramtui"
+  version "1.0.0"
+
+  url "https://github.com/k4dy/telegramtui/releases/download/v#{version}/telegramtui-#{version}.jar"
+  sha256 "a65d7f2dc5fb729cf707bed3b5e070983d4d5d8bd6348796ffc84f56c4a2a507"
+
+  depends_on "openjdk@21"
+  depends_on "tdlib"
+
+  def install
+    libexec.install "telegramtui-#{version}.jar" => "telegramtui.jar"
+
+    tdlib_lib = Formula["tdlib"].opt_lib
+    java_bin  = Formula["openjdk@21"].opt_bin/"java"
+
+    (bin/"telegramtui").write <<~EOS
+      #!/bin/bash
+      exec "#{java_bin}" \
+        -Djna.library.path="#{tdlib_lib}" \
+        -jar "#{libexec}/telegramtui.jar" "$@"
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"telegramtui", :exist?
+    assert_predicate libexec/"telegramtui.jar", :exist?
+  end
+end
